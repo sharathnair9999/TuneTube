e561ae38603a530a48c222aeb928871c245911b3
Sharath <sharathnair9999@gmail.com>
2022-04-19 08:49:34 +0530

    videos data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,92 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "kFz9afj8lu0",
+    title: "Can You Actually Game in 8K? (RTX 3090 Gameplay!)",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Gaming in 8K with an RTX3090 is surprisingly impressive! Uploaded in 8K. RIP to your GPU. *8K may or may not take up to a week to process lol",
+    creator: "Marques Brownlee",
+    uploadedOn: " Sep 23, 2020",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "f4g2nPY-VZc",
+    title: "Apple M1 Mac Review: Time to Recalibrate!",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "The Macbook with M1 chip is... different. Time to think a little different about laptops.",
+    creator: "Marques Brownlee",
+    uploadedOn: "Nov 17, 2020",
   },
+  {
+    _id: "788E-ATJ3pk",
+    title: "I bought every iPhone ever.",
+    description:
+      "With iPhone 11 / iPhone 11 Pro upon us, welcome to the Ultimate iPhone Unboxing + Camera, Speed, and Display Comparison reviews!",
+    creator: "Marques Brownlee",
+    uploadedOn: "Oct 5, 2019",
+  },
+  {
+    _id: "UdfSrJvqY_E",
+    title: "AirPods Max Unboxing & Impressions: $550?!",
+    description:
+      "AirPods Max: Apple just dropped a surprise set of $549 ultra premium noise cancelling headphones. This is everything you need to know!",
+    creator: "Mrwhosetheboss",
+    uploadedOn: "Dec 10, 2020",
+  },
+  {
+    _id: "zDG-n8JN_78",
+    title: "The $200,000 Smartphone Unboxing 💀",
+    description:
+      "You'll see lots of smartphone unboxings in 2021 - Samsung Galaxy unboxings, iPhone 13 / 12s unboxings etc, but none will compare to the ridiculousness of this $200,000 smartphone unboxing. ",
+    creator: "Mrwhosetheboss",
+    uploadedOn: "Apr 29, 2021",
+  },
+  {
+    _id: "wGk8du2VVCs",
+    title: "$7 Smartphone vs $7,000 Smartphone.",
+    description:
+      "How bad can a smartphone be in 2020?  How Good can a smartphone be in 2020? Let's find out. ",
+    creator: "Mrwhosetheboss",
+    uploadedOn: "Jul 5, 2020",
+  },
+  {
+    _id: "7eQG8mRlYHM",
+    title: "Galaxy Note 20 Ultra Durability Test - What is 'Victus' Glass?",
+    description:
+      "The Galaxy Note 20 Ultra is the first smartphone to use Gorilla Glass Victus on the display... And its time we find out how scratch resistant that new Victus Glass really is. Is this new Note 20 Ultra worth the Ultra price? ",
+    creator: "JerryRigEverything",
+    uploadedOn: "Aug 17, 2020",
+  },
+  {
+    _id: "Dqi9QGyRWGk",
+    title: "The ROG Phone 5 has a Problem - Durability Test Fail!",
+    description:
+      "The ROG phone 5 is a pretty expensive and powerful gaming smartphone. Its lost the iconic aerodynamic hump from the previous 3 versions of the phone, and now the ROG 5 has added dual batteries so it can charge faster. But will that compromise the Structural integrity of the ROG 5? I mean... yeah. You've seen the thumbnail. The ROG Phone five definitely has some structural issues....",
+    creator: "JerryRigEverything",
+    uploadedOn: "Mar 17, 2021",
+  },
+  {
+    _id: "sCPq9Jwgfvg",
+    title: "10 Gadgets Under ₹50 That Are Actually Useful!",
+    description:
+      " When we did a video on the best gadgets under 100 rupees, a lot of you commented that you can buy a lot of gadgets under 50 rupees offline. So, we went in Nehru Place, a big electronics market in Delhi, and we found a ton of gadgets under 50 rupees.",
+    creator: "Beebom",
+    uploadedOn: "Apr 9, 2022",
+  },
+  {
+    _id: "ZnZ5IuhhIIs",
+    title: "OnePlus Watch Review: Worth the Price?",
+    description:
+      "After lots of leaks and rumors, OnePlus unveiled the OnePlus Watch alongside the OnePlus 9 series. Well, it’s finally here. We have been testing the OnePlus Watch for over a week and this is our OnePlus Watch review. In this video, we test the OnePlus Watch display, battery, fitness features, smart features and compare the OnePlus Watch vs OPPO Watch vs Amazfit GTR 2.",
+    creator: "Beebom",
+    uploadedOn: "Apr 13, 2021",
+  },
+  {
+    _id: "T8AS4gWDPqU",
+    title: "Dell XPS 13 (2018): The Bezel-less 4K Beauty!",
+    description:
+      " The new Dell XPS 13 was announced at CES 2018 and it's here. This is not the Dell XPS 13 review, instead we take a quick look at the Dell XPS 13 and its beautiful 4K display. ",
+    creator: "Beebom",
+    uploadedOn: "Apr 21, 2018",
+  },
+
 ];
