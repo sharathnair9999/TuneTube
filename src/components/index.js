a87e3517b4dc76b07a35903af5e90b7826dfd782
Sharath <sharathnair9999@gmail.com>
2022-04-21 10:51:47 +0530

    landing page structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import VideoCard from "./VideoCard/VideoCard";
 import HorizontalCard from "./HorizontalCard/HorizontalCard";
 import Alert from "./Alert/Alert";
+import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
 
-export {VideoCard, HorizontalCard, Alert}
+export {VideoCard, HorizontalCard, Alert, NavBar, Footer}
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,10 +1,263 @@
-import React from 'react'
-import "./Landing.css"
+import React from "react";
+import "./Landing.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div>
+      <div>
+        1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita a.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita alast.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita alast.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita alast.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ea
+        vitae, iste distinctio doloribus ullam dolore, sint praesentium culpa
+        unde placeat illum, enim voluptatem sapiente voluptate magnam! Adipisci,
+        expedita alastone.
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
