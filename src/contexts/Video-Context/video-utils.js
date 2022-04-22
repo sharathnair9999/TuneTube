b898858d9b5c4a0e9df47e6dad14db3c7718a850
Sharath <sharathnair9999@gmail.com>
2022-04-22 23:45:03 +0530

    Feat : Added Category Chip on Video listing page

--- src/contexts/Video-Context/video-utils.js
+++ src/contexts/Video-Context/video-utils.js
@@ -1,6 +1,6 @@
 export const initialVideosState = {
   allVideos: [],
-  currVideo : {},
+  currVideo: {},
   allCategories: [],
   filters: {
     dateSort: "LATEST",
@@ -12,12 +12,29 @@ export const initialVideosState = {
 export const videosReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
+    case "GET_ALL_CATEGORIES":
+      return { ...state, allCategories: payload };
+    case "FILTER_BY_CATEGORY":
+      if (
+        state.filters.filterByCategory === payload &&
+        state.filters.filterByCategory === "All"
+      ) {
+        return {
+          ...state,
+          filters: { ...state.filters, filterByCategory: "" },
+        };
+      }
+      return {
+        ...state,
+        filters: { ...state.filters, filterByCategory: payload },
+      };
+
     case "LATEST":
       return { ...state, filters: { ...state.filters, dateSort: "LATEST" } };
     case "OLDEST":
       return { ...state, filters: { ...state.filters, dateSort: "OLDEST" } };
     case "CURRENT_VIDEO":
-      return {...state, currVideo : payload}
+      return { ...state, currVideo: payload };
     default:
       return initialVideosState;
   }
