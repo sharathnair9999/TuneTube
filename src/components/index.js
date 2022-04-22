c4a808e6542bbfc3696a0d8b8805b8772d5c751a
Sharath <sharathnair9999@gmail.com>
2022-04-22 11:13:39 +0530

    Added hero img url

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import HorizontalCard from "./HorizontalCard/HorizontalCard";
 import Alert from "./Alert/Alert";
 import NavBar from "./NavBar/NavBar";
 import Footer from "./Footer/Footer";
+import Categories from "./Categories/Categories"
 
-export {VideoCard, HorizontalCard, Alert, NavBar, Footer}
+export {VideoCard, HorizontalCard, Alert, NavBar, Footer, Categories}
