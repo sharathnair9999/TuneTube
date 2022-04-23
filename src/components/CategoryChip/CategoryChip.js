db3b8dad1f111d1971d29226297e4300e814e2a1
Sharath <sharathnair9999@gmail.com>
2022-04-23 17:18:45 +0530

    Fix: Category chip

--- src/components/CategoryChip/CategoryChip.js
+++ src/components/CategoryChip/CategoryChip.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useVideos } from "../../contexts";
 import "./CategoryChip.css";
 
@@ -10,13 +10,14 @@ const CategoryChip = ({ category }) => {
   } = videosState;
 
   const selectCategory = (cat) => {
+    console.log("category", cat);
     videosDispatch({ type: "FILTER_BY_CATEGORY", payload: cat });
   };
 
   return (
     <button
       onClick={() => selectCategory(category)}
-      className={`chip ${filterByCategory===category && 'active'}`}
+      className={`chip ${filterByCategory === category && "active"}`}
     >
       {category}
     </button>
