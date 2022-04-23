cab9734b821d3c3d8f3750ef80a045a1282cd143
Sharath <sharathnair9999@gmail.com>
2022-04-23 18:20:12 +0530

    Empty Data Image and text

--- src/app-utils/constants.js
+++ src/app-utils/constants.js
@@ -1,6 +1,7 @@
 export const constants = {
   imgUrls : {
     logo : "https://res.cloudinary.com/sharath-media-library/image/upload/v1650477413/unbox%20tube/UnboxTube-logos_transparent_nf40e2.png",
-    landing_hero_img : "https://res.cloudinary.com/sharath-media-library/image/upload/v1650564448/unbox%20tube/unbox-3_uwgrou.jpg"
+    landing_hero_img : "https://res.cloudinary.com/sharath-media-library/image/upload/v1650564448/unbox%20tube/unbox-3_uwgrou.jpg",
+    empty_box: "https://res.cloudinary.com/sharath-media-library/image/upload/v1650717624/unbox%20tube/empty_box_wtmfxc.png"
   }
 }
