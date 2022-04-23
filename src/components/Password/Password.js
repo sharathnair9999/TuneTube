8fe80df337f27ac57294d5ac93f32571dc1ed5c3
Sharath <sharathnair9999@gmail.com>
2022-04-23 23:45:03 +0530

    Fix : Added tooltip to Show/hide password btn

--- src/components/Password/Password.js
+++ src/components/Password/Password.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./Password.css";
+import ReactTooltip from "react-tooltip";
 import { BiShow, BiHide } from "react-icons/bi";
 
 const Password = ({ handleChange, fieldValue, fieldName, ...other }) => {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="password">
+      <ReactTooltip place="bottom" effect="solid" />
       <input
         type={`${showPassword ? "text" : "password"}`}
         required
@@ -17,6 +19,7 @@ const Password = ({ handleChange, fieldValue, fieldName, ...other }) => {
         {...other}
       />
       <span
+        data-tip={showPassword ? "Hide" : "Show"}
         className="show-password pointer"
         onClick={() => setShowPassword(!showPassword)}
       >
