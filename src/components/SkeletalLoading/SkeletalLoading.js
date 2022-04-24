c7d207d940b66e57ec61b11d685dc5b55ebfbe2e
Sharath <sharathnair9999@gmail.com>
2022-04-24 13:24:41 +0530

    Skeletal Loading fix

--- src/components/SkeletalLoading/SkeletalLoading.js
+++ src/components/SkeletalLoading/SkeletalLoading.js
@@ -6,7 +6,7 @@ const SkeletalLoading = () => {
   return (
       <div className="listing-skeleton">
         <div className="image skeleton"></div>
-        <div className="contents flex justify-space-btw items-center gap-sm">
+        <div className="contents flex-and-center gap-sm">
           <div className="avatar skeleton"></div>
           <div className="text-section flex-and-center flex-col gap-sm">
             <div className="text skeleton"></div>
