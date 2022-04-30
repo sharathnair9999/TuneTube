ae39db7501952c7323540dea7a91471a4281f859
Sharath <sharathnair9999@gmail.com>
2022-05-01 00:03:49 +0530

    Feat : Horizontal Card for Liked Video page

--- src/components/HorizontalCard/HorizontalCard.js
+++ src/components/HorizontalCard/HorizontalCard.js
@@ -1,11 +1,39 @@
-import './HorizontalCard.css'
+import "./HorizontalCard.css";
 
-import React from 'react'
+import React from "react";
+import { useVideos } from "../../contexts";
+import { IoEllipsisVerticalOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-const HorizontalCard = () => {
+const HorizontalCard = ({ video }) => {
+  const { getThumbnail } = useVideos();
+  const { _id, title, creator, creatorImg } = video;
+  const thumbnai = getThumbnail(_id);
   return (
-    <div>HorizontalCard</div>
-  )
-}
+    <div className="like-card flex justify-fs items-fs  w-100">
+      <Link to={`/explore/${_id}`}>
+        <img src={thumbnai} alt={title} className="responsive-img" />
+      </Link>
+      <div className="card-text ">
+        <p className="card-title">
+          <Link to={`/explore/${_id}`}>{title}</Link>
+        </p>
+        <section className="creator-info flex justify-fs items-center">
+          <img
+            src={creatorImg}
+            alt={creator}
+            className="creator-img responsive-img"
+          />
+          <small>{creator}</small>
+        </section>
+      </div>
+      <section className="video-actions-container">
+      <button className="btn-transparent video-actions-btn">
+        <IoEllipsisVerticalOutline color="white" size={"1rem"} />
+      </button>
+      </section>
+    </div>
+  );
+};
 
-export default HorizontalCard
+export default HorizontalCard;
