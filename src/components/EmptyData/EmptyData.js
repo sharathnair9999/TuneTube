351caee6a58c1f6f320b5a8a0cb1db2e7c67f73e
Sharath <sharathnair9999@gmail.com>
2022-05-01 13:52:39 +0530

    Empty Data Component Fix - can be ignored

--- src/components/EmptyData/EmptyData.js
+++ src/components/EmptyData/EmptyData.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { constants } from "../../app-utils";
 import "./EmptyData.css";
 
-const EmptyData = ({ msg, imgUrl = constants.imgUrls.empty_box }) => {
+const EmptyData = ({ msg, imgUrl = constants.imgUrls.empty_box, url = "" }) => {
   return (
-    <div className="w-100 flex-and-center flex-col">
-      <img src={imgUrl} alt="empty-box" />
-      <p className="font-xl">{msg}</p>
+    <div className="empty-data-container w-100 flex-and-center flex-col">
+      <img src={imgUrl} alt="empty-box " className="responsive-img" />
+      <p className="msg-text">{msg}</p>
+      {url !== "" && <Link className="mt-1 link" to={url}>Explore All Videos</Link>}
     </div>
   );
 };
