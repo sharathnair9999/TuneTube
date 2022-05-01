eccaceeb87bbda4b6aa335f0b1cde07b41966661
Sharath <sharathnair9999@gmail.com>
2022-05-01 13:24:45 +0530

    Fixed Liking issue

--- src/components/VideoActions/Like.js
+++ src/components/VideoActions/Like.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth, useVideos } from "../../contexts";
+import { useAuth } from "../../contexts";
 import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 
-const Like = () => {
-  const navigate = useNavigate();
+const Like = ({ video }) => {
   const {
     addToLikedVideos,
     removeFromLikedVideos,
     userState: { likedVideos, isLoggedIn },
   } = useAuth();
-  const {
-    videosState: { currVideo },
-  } = useVideos();
 
-  const isVideoLiked = likedVideos.some((video) => video._id === currVideo._id);
-  const handleLike = (video) => {
-    isLoggedIn
-      ? !isVideoLiked
-        ? addToLikedVideos(video)
-        : removeFromLikedVideos(currVideo._id)
-      : navigate("/login");
+  const isVideoLiked = likedVideos.some(
+    (likedVideo) => likedVideo._id === video._id
+  );
+  const handleLike = () => {
+    !isVideoLiked ? addToLikedVideos(video) : removeFromLikedVideos(video._id);
   };
   return (
-    <button className="btn-transparent" onClick={() => handleLike(currVideo)}>
+    <button className="btn-transparent" onClick={() => handleLike()}>
       {isLoggedIn ? (
         !isVideoLiked ? (
           <AiOutlineLike color="white" size={"1.2rem"} />
