8a9ca23ba2e3b7a5aff391d17c802010955c9584
Sharath <sharathnair9999@gmail.com>
2022-06-10 10:45:54 +0530

    Navbar fixed in mobile layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { constants } from "../../app-utils";
-import { useClickOutside } from "../../custom-hooks";
 import "./NavBar.css";
-import { GiHamburgerMenu } from "react-icons/gi";
-import {
-  AiOutlineCloseCircle,
-  AiFillLike,
-  AiOutlineHistory,
-} from "react-icons/ai";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import { MdExplore, MdWatchLater } from "react-icons/md";
-import { ImList } from "react-icons/im";
+import { Link, useNavigate } from "react-router-dom";
+
 import { useAuth } from "../../contexts";
 import UserAvatar from "../UserAvatar/UserAvatar";
 
 const NavBar = () => {
-  const { ref, isComponentVisible, setIsComponentVisible } =
-    useClickOutside(false);
   const navigate = useNavigate();
 
   const {
@@ -24,43 +14,8 @@ const NavBar = () => {
     logoutUser,
   } = useAuth();
 
-  const navLinks = [
-    {
-      link: "/explore",
-      linkName: "Explore",
-      icon: <MdExplore size={"1.2rem"} />,
-    },
-    { link: "/liked", linkName: "Liked", icon: <AiFillLike size={"1.2rem"} /> },
-    {
-      link: "/playlists",
-      linkName: "Playlists",
-      icon: <ImList size={"1.2rem"} />,
-    },
-    {
-      link: "/history",
-      linkName: "History",
-      icon: <AiOutlineHistory size={"1.2rem"} />,
-    },
-    {
-      link: "/watch-later",
-      linkName: "Watch Later",
-      icon: <MdWatchLater size={"1.2rem"} />,
-    },
-  ];
-
   return (
-    <div className={`navbar-container`} ref={ref}>
-      <section className={`open-btn-section`}>
-        <button
-          className="nav-toggler"
-          onClick={() => {
-            setIsComponentVisible(!isComponentVisible);
-          }}
-        >
-          <GiHamburgerMenu />
-        </button>
-        <span className="border"></span>
-      </section>
+    <div className={`navbar-container`}>
       <Link
         className="logo-container flex justify-fs items-center gap-xsm"
         to={"/"}
@@ -90,46 +45,6 @@ const NavBar = () => {
           isLoggedIn={isLoggedIn}
         />
       )}
-
-      <aside
-        className={`${
-          isComponentVisible ? "show-side-nav" : "hide-side-nav"
-        } navbar`}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        <nav>
-          <section className="close-btn-section ">
-            <button
-              className="nav-toggler"
-              onClick={() => setIsComponentVisible(!isComponentVisible)}
-            >
-              <AiOutlineCloseCircle />
-            </button>
-          </section>
-          <ul className="options">
-            {navLinks?.map(({ link, linkName, icon }) => (
-              <li
-                key={link}
-                onClick={() => setIsComponentVisible(!isComponentVisible)}
-              >
-                <NavLink
-                  className={({ isActive }) =>
-                    `nav-link ${
-                      isActive && "active"
-                    } flex justify-fs items-center`
-                  }
-                  to={link}
-                >
-                  {icon}
-                  <span>{linkName}</span>
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </aside>
     </div>
   );
 };
---
+++ src/components/NavBar/SideNav.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  AiOutlineCloseCircle,
+  AiFillLike,
+  AiOutlineHistory,
+} from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import { MdExplore, MdWatchLater } from "react-icons/md";
+import { ImList } from "react-icons/im";
+import { useClickOutside } from "../../custom-hooks";
+
+const SideNav = () => {
+  const { ref, isComponentVisible, setIsComponentVisible } =
+    useClickOutside(false);
+
+  const navLinks = [
+    {
+      link: "/explore",
+      linkName: "Explore",
+      icon: <MdExplore size={"1.2rem"} />,
+    },
+    { link: "/liked", linkName: "Liked", icon: <AiFillLike size={"1.2rem"} /> },
+    {
+      link: "/playlists",
+      linkName: "Playlists",
+      icon: <ImList size={"1.2rem"} />,
+    },
+    {
+      link: "/history",
+      linkName: "History",
+      icon: <AiOutlineHistory size={"1.2rem"} />,
+    },
+    {
+      link: "/watch-later",
+      linkName: "Watch Later",
+      icon: <MdWatchLater size={"1.2rem"} />,
+    },
+  ];
+
+  return (
+    <div ref={ref}>
+      <section className={`open-btn-section`}>
+        <button
+          className="nav-toggler"
+          onClick={() => {
+            setIsComponentVisible(!isComponentVisible);
+          }}
+        >
+          <GiHamburgerMenu />
+        </button>
+        <span className="border"></span>
+      </section>
+      <aside
+        className={`${
+          isComponentVisible ? "show-side-nav" : "hide-side-nav"
+        } navbar`}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <nav>
+          <section className="close-btn-section ">
+            <button
+              className="nav-toggler"
+              onClick={() => setIsComponentVisible(!isComponentVisible)}
+            >
+              <AiOutlineCloseCircle />
+            </button>
+          </section>
+          <ul className="options">
+            {navLinks?.map(({ link, linkName, icon }) => (
+              <li
+                key={link}
+                onClick={() => setIsComponentVisible(!isComponentVisible)}
+              >
+                <NavLink
+                  className={({ isActive }) =>
+                    `nav-link ${
+                      isActive && "active"
+                    } flex justify-fs items-center`
+                  }
+                  to={link}
+                >
+                  {icon}
+                  <span>{linkName}</span>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </aside>
+    </div>
+  );
+};
+
+export default SideNav;
